0458d4674dd021c3513c886819160d889f63cbea
addna <addna@DESKTOP-MFNARD1>
2019-07-19 11:52:49 +0200

    Navigation and Login form

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -44,7 +44,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLogin userForLogin)
         {
-            var userFromRepo = await _repo.Login(userForLogin.Username.ToLower(), userForLogin.Password);
+            var userFromRepo = await _repo.Login(userForLogin.Username, userForLogin.Password);
 
             if (userFromRepo == null)
                 return Unauthorized();
