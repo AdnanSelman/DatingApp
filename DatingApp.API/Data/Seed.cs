353c74f6846fa9b1f389d12d1498cb28eadaf233
addna <addna@DESKTOP-MFNARD1>
2019-07-21 22:34:20 +0200

    Seeding Data on the Database

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _conetext;
+        public Seed(DataContext conetext)
+        {
+            _conetext = conetext;
+        }
+
+        public void SeedUsers()
+        {
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+            foreach (var user in users)
+            {
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Username = user.Username.ToLower();
+
+                _conetext.Users.Add(user);
+            }
+
+            _conetext.SaveChanges();
+        }
+
+     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
